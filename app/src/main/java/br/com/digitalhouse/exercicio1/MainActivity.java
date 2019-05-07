9d8e333ab3a51d8aaae48eed18780747db75afc4
unknown <Digital@DH-A8-29.dhbrasil.digitalhouse.com>
2019-05-06 22:23:34 -0300

    Initial commit

---
+++ app/src/main/java/br/com/digitalhouse/exercicio1/MainActivity.java
@@ -0,0 +1,27 @@
+package br.com.digitalhouse.exercicio1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText Texto_center;
+    Button btnEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Texto_center = findViewById(R.id.)
+
+
+
+
+
+    }
+}
